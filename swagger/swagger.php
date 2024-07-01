5c05eb46d9ea56d9d804b4e0b6fccbcb45d6e464
Jules Roche <julesro42@gmail.com>
2024-07-01 11:40:15 +0200

    create swagger API

---
+++ swagger/swagger.php
@@ -0,0 +1,14 @@
+<?php
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="Laravel API",
+ *     version="1.0.0"
+ * )
+ * @OA\Server(
+ *    url="http://localhost:8000",
+ *    description="Development server"
+ * )
+ * **/
