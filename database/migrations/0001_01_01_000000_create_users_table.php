1482035549eb215efce4258c288cc18a2db80ee9
Jules Roche <julesro42@gmail.com>
2024-07-01 11:21:58 +0200

    modif model user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('nom');
             $table->string('poste');
             $table->int('status')->default(0);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
