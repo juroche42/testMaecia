1068b108b7ff7f6e067c5ccac8498f4c1742e84f
Jules Roche <julesro42@gmail.com>
2024-07-02 14:08:21 +0200

    route users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+
+    /**
+     * @OA\Get(
+     *     path="/api/users",
+     *     summary="Get all users",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(type="array", @OA\Items(ref="#/components/schemas/User"))
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Server error"
+     *     )
+     * )
+     */
+    public function index()
+    {
+        $users = User::with('services:name')
+            ->select(['id','prenom', 'nom', 'poste'])
+            ->get();
+        return response()->json($users);
+    }
 }
