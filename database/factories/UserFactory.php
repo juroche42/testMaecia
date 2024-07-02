76b45cb35797b61eb5aec3f40f80b08b0b36da4e
Jules Roche <julesro42@gmail.com>
2024-07-02 11:36:55 +0200

    config userFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,21 +24,11 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'nom' => fake()->lastName(),
+            'prenom' => fake()->firstName(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'poste' => fake()->jobTitle(),
+            'status' => fake()->boolean() ? 1 : 0,
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
